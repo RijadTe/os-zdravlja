d4dbab2b104518502449a14dd673f9951e24da29
Tvoje Rijad Terzic <rijad.te1@gmail.com>
2026-08-03 18:34:46 +0200

    Dodan home prevod za hrvatski jezik

--- frontend/src/i18n/index.js
+++ frontend/src/i18n/index.js
@@ -2,27 +2,26 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
-import Backend from 'i18next-http-backend';
+
+import hrTranslation from '../../public/locales/hr/translation.json';
+import enTranslation from '../../public/locales/en/translation.json';
+import deTranslation from '../../public/locales/de/translation.json';
+
+const resources = {
+  hr: { translation: hrTranslation },
+  en: { translation: enTranslation },
+  de: { translation: deTranslation },
+};
 
 i18n
-  // Učitava prevode iz public/locales
-  .use(Backend)
-  // Detektuje jezik preglednika
   .use(LanguageDetector)
-  // Povezuje sa React-om
   .use(initReactI18next)
   .init({
-    fallbackLng: 'hr', // Ako prevod ne postoji, prikaži hrvatski
-    debug: false, // Postavi na true za debug (prikazuje greške u konzoli)
+    resources,
+    fallbackLng: 'hr',
+    debug: true, // ← DODAJ OVO ZA DEBUG!
     interpolation: {
-      escapeValue: false, // React već štiti od XSS napada
-    },
-    backend: {
-      loadPath: '/locales/{{lng}}/translation.json',
-    },
-    detection: {
-      order: ['localStorage', 'navigator'],
-      caches: ['localStorage'], // Pamti izbor jezika
+      escapeValue: false,
     },
   });
 
